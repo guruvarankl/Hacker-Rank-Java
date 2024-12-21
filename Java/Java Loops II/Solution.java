bc98a391fe025defc4e16b2c1e18ff0f620b1d13
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2024-12-21 11:51:27 +0530

    Update Solution.java

--- Java/Java Loops II/Solution.java
+++ Java/Java Loops II/Solution.java
@@ -1,1 +1,21 @@
+import java.util.*;
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int k;
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            k=0;
+            for(int j=0;j<n;j++){
+                k=k+(((int)Math.pow(2,j))*b);
+                System.out.print(k+a +" ");
+            }
+            System.out.println();
+       }
+        in.close();
+    }
+}
 
