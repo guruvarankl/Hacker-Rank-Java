61cc013baddf1d8e04fd85930ece2efe600399b4
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2024-12-21 11:55:51 +0530

    Update Solution.java

--- Java/Java Datatypes/Solution.java
+++ Java/Java Datatypes/Solution.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n;    
+        n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            try{
+                long x=sc.nextLong();
+                System.out.println(x+ " can be fitted in:");
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)
+                    System.out.println("* byte");
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)
+                    System.out.println("* short");
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)
+                    System.out.println("* int");
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)
+                    System.out.println("* long");               
+            }
+            catch(Exception E){             
+                System.out.println(sc.next()+ " can't be fitted anywhere.");
+            }                          
+           }    
+      }
+}
 
