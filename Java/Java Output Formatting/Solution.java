41b133269c4ea8b3d095d51cd4e5eafe059c02df
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2024-12-21 11:46:04 +0530

    Update Solution.java

--- Java/Java Output Formatting/Solution.java
+++ Java/Java Output Formatting/Solution.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                for(int j=s1.length();j<15;j++)
+                s1=s1 + ' ';
+                int num=sc.nextInt();
+                System.out.print(s1);
+                System.out.printf("%03d\n",num);
+            }
+            System.out.println("================================");    
+    }
+}
+
 
