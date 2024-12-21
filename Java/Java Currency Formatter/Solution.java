e261a78b9fe7611ba2675a624e541a7a59e17213
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2024-12-21 11:59:08 +0530

    Update Solution.java

--- Java/Java Currency Formatter/Solution.java
+++ Java/Java Currency Formatter/Solution.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+import java.text.*;
+class Solution{   
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Double abc;
+        abc=sc.nextDouble();
+        System.out.println("US: " + currency(Locale.US,abc));
+        System.out.println("India: " + currency(new Locale("en","IN"),abc));
+        System.out.println("China: " + currency(Locale.CHINA,abc));
+        System.out.println("France: " + currency(Locale.FRANCE,abc));
+         }
+    public static String currency(Locale l1,Double D1){
+        NumberFormat abc=NumberFormat.getCurrencyInstance(l1);
+        String s1=abc.format(D1);
+        return s1;
+    }
+}
