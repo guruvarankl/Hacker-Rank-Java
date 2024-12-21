ca6d4688d3ba004d0450ed34e0ed3c4e67498b35
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2024-12-21 11:40:13 +0530

    Update Solution.java

--- Java/Java If-Else/Solution.java
+++ Java/Java If-Else/Solution.java
@@ -1,1 +1,18 @@
+import java.util.*;
+public class Solution {
+public static void main(String args[])
+{
+    Scanner s1=new Scanner(System.in);
+    int s= s1.nextInt();
+    if((s%2)!=0)
+    System.out.println("Weird");
+    else
+    if((s>=2 && s<=5) || s>20)
+        System.out.println("Not Weird");
+    else
+        if(s>=6 && s<=20)  System.out.println("Weird");
+    s1.close();
+    
+}   
+}
 
