345eeb1a8c77a8c2b1d9c3b8927222f1ee20c5d2
guruvarankl <43952156+guruvarankl@users.noreply.github.com>
2025-01-24 09:18:14 +0530

    Update Solution.java

--- Java/Java End-of-file/Solution.java
+++ Java/Java End-of-file/Solution.java
@@ -1,1 +1,14 @@
-
+import java.io.*;
+import java.util.*;
+class Solution{
+public static void main(String args[])
+{
+    String s1;
+    int i=1;
+    Scanner s=new Scanner(System.in);
+    while(s.hasNext())
+    {
+      System.out.println(i++ +" "+ s.nextLine());
+    }
+      
+}}
